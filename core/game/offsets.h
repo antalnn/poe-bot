e3aa4bfcf792c3e09062b15979f36705e47c5c9c
tonik <thehaxorcz@gmail.com>
2023-11-04 11:01:49 +0100

    Updated for 3.22.2 hotfix 3

--- core/game/offsets.h
+++ core/game/offsets.h
@@ -4,8 +4,8 @@ namespace offsets
 {
 
 	// Steam; Patch: 3.22.1d
-	constexpr uint32_t player_base_address = 0x02E4F258;
-	constexpr uint32_t zoom_base_address = player_base_address + 0x310;
+	constexpr uint32_t player_base_address = 0x2E51268; // ff0 0x02E50248
+	constexpr uint32_t zoom_base_address = player_base_address + 0x320;
 
 	// Netvars
 	constexpr uint32_t m_iHealth[4] = { 0x58, 0x50, 0x48, 0x278 };
